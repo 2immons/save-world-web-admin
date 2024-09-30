809bceb67bead2e06ec37e767a800cfd1f1bcaa2
2immons <podoprigoraps@gmail.com>
2024-09-30 19:35:07 +0400

    Normalize line endings

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:
-      process.env.NODE_ENV === "production"
-          ? "https://2immons.github.io/save-world-web-admin/"
-          : "/",
+    process.env.NODE_ENV === "production"
+      ? "https://2immons.github.io/save-world-web-admin/"
+      : "/",
 });
